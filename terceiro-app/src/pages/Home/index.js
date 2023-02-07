596a38ca59c86153ef1bae46d548e835ef7864cd
Danilo Amorim <danbamorim@gmail.com>
2023-02-06 21:27:34 -0300

    rotas adicionado

---
+++ terceiro-app/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+
+function Home() {
+    return (
+      <div>
+        <h1>TEste o homi</h1>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
---
+++ terceiro-app/src/pages/Sobre/index.js
@@ -0,0 +1,10 @@
+function Sobre() {
+    return (
+      <div>
+        <h1>TEste o Sobre</h1>
+      </div>
+    );
+  }
+  
+ 
+  export default Sobre;
